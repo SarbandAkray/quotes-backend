d35fa6df8c0b5d11f79800ed4235b83a47f24e21
Sarband <sarbandmuhamad@gmail.com>
2023-12-21 13:01:22 +0300

    port fixed

--- index.js
+++ index.js
@@ -1,75 +1,61 @@
-const express = require('express');
-const { json } = require('express/lib/response');
+const express = require("express");
+const { json } = require("express/lib/response");
 const app = express();
-const PORT = 8080;
-const fs = require('fs');
+const PORT = 3001;
+const fs = require("fs");
 
-app.listen(process.env.PORT ||PORT);
+app.listen(process.env.PORT || PORT);
 
-
-app.use(express.static(__dirname+"/pages"));
+app.use(express.static(__dirname + "/pages"));
 
 //getting desktop version
-app.get("/getVersion", (req,res)=>{
-res.status(200).send({
-    version: "1.0.0"
-});
+app.get("/getVersion", (req, res) => {
+  res.status(200).send({
+    version: "1.0.0",
+  });
 });
 
 //getting android
-app.get("/getVersion/android", (req,res)=>{
-    res.status(200).send({
-        version: "1.0.0"
+app.get("/getVersion/android", (req, res) => {
+  res.status(200).send({
+    version: "1.0.0",
+  });
 });
-});
 
-app.post("/download/desktop" ,(req,res)=>{
-    
-    fs.readFile('desktopDownloads.json', 'utf8', function(err, data){
-        let jsonData = JSON.parse(data);
-        let getAmount = jsonData[0].amount;
-        getAmount= getAmount+1;
-        let NewData = [{amount:getAmount}];
-        let goodData = JSON.stringify(NewData);
-        fs.writeFile('desktopDownloads.json', goodData, 'utf8',()=>{});
-    });
-
-
+app.post("/download/desktop", (req, res) => {
+  fs.readFile("desktopDownloads.json", "utf8", function (err, data) {
+    let jsonData = JSON.parse(data);
+    let getAmount = jsonData[0].amount;
+    getAmount = getAmount + 1;
+    let NewData = [{ amount: getAmount }];
+    let goodData = JSON.stringify(NewData);
+    fs.writeFile("desktopDownloads.json", goodData, "utf8", () => {});
+  });
 });
 
 // get the amount of downloads for windows
-app.get("/get/downalodsForWindows" ,(req,res)=>{
-    
-    fs.readFile('desktopDownloads.json', 'utf8', function(err, data){
-        let jsonData = JSON.parse(data);
-        res.send(jsonData);
-    });
-
-
+app.get("/get/downalodsForWindows", (req, res) => {
+  fs.readFile("desktopDownloads.json", "utf8", function (err, data) {
+    let jsonData = JSON.parse(data);
+    res.send(jsonData);
+  });
 });
 
-
-app.post("/download/android" ,(req,res)=>{
-    
-    fs.readFile('androidDownloads.json', 'utf8', function(err, data){
-        let jsonData = JSON.parse(data);
-        let getAmount = jsonData[0].amount;
-        getAmount= getAmount+1;
-        let NewData = [{amount:getAmount}];
-        let goodData = JSON.stringify(NewData);
-        fs.writeFile('androidDownloads.json', goodData, 'utf8',()=>{});
-    });
-
-
+app.post("/download/android", (req, res) => {
+  fs.readFile("androidDownloads.json", "utf8", function (err, data) {
+    let jsonData = JSON.parse(data);
+    let getAmount = jsonData[0].amount;
+    getAmount = getAmount + 1;
+    let NewData = [{ amount: getAmount }];
+    let goodData = JSON.stringify(NewData);
+    fs.writeFile("androidDownloads.json", goodData, "utf8", () => {});
+  });
 });
 
 // get amout of donwlaods for android
-app.get("/get/downalodsForAndroid" ,(req,res)=>{
-    
-    fs.readFile('androidDownloads.json', 'utf8', function(err, data){
-        let jsonData = JSON.parse(data);
-        res.send(jsonData);
-    });
-
-
+app.get("/get/downalodsForAndroid", (req, res) => {
+  fs.readFile("androidDownloads.json", "utf8", function (err, data) {
+    let jsonData = JSON.parse(data);
+    res.send(jsonData);
+  });
 });
